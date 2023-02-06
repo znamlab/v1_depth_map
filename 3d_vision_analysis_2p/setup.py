35ab67bcbfd19743fb6cccb6db74cff973dd47d7
Antonin Blot <antonin.blot@crick.ac.uk>
2023-02-06 10:55:56 +0000

    Add setup.py

---
+++ 3d_vision_analysis_2p/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='3d_vision_analysis_2p',
+    version='v0.1',
+    packages=find_packages(),
+    url='https://github.com/znamlab/cottage_analysis',
+    license='MIT',
+    author='Antonin Blot, Yiran He, Petr Znamenskyi',
+    author_email='yiran.he@crick.ac.uk',
+    description='Analysis of the V1 depth dataset'
+)
