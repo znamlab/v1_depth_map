88e949a28668a9ca0d7294f8efa19c73dff4c3b5
yiran25 <yiran.he@crick.ac.uk>
2024-02-29 14:25:45 +0000

    [feature] add plots for depth decoder

---
+++ v1_depth_analysis/v1_manuscript_2023/depth_decoder.py
@@ -0,0 +1,189 @@
+import functools
+print = functools.partial(print, flush=True)
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42 # for pdfs
+import matplotlib.pyplot as plt
+from pathlib import Path
+from tqdm import tqdm
+import scipy
+from scipy.stats import ttest_rel
+import itertools
+
+import flexiznam as flz
+from cottage_analysis.pipelines import pipeline_utils
+from cottage_analysis.plotting import plotting_utils, depth_decoder_plots
+
+
+def concatenate_all_decoder_results(flexilims_session, session_list, filename="decoder_results.pickle"):
+    isess=0
+    results = pd.DataFrame(
+        columns=[
+            "session",
+            "accuracy_closedloop",
+            "accuracy_openloop",
+            "conmat_closedloop",
+            "conmat_openloop",
+        ]
+    )
+    results["conmat_closedloop"] = [[np.nan]]
+    results["conmat_openloop"] = [[np.nan]]
+    
+    for session in session_list:
+        neurons_ds = pipeline_utils.create_neurons_ds(
+        session_name=session,
+        flexilims_session=flexilims_session,
+        project=None,
+        conflicts="skip",
+        )
+        
+        filepath = neurons_ds.path_full.parent/filename
+        if filepath.is_file():
+            decoder_dict = pd.read_pickle(neurons_ds.path_full.parent/filename)
+            results.at[isess, "session"] = session
+            results.at[isess, "accuracy_closedloop"] = decoder_dict["accuracy_closedloop"]
+            results.at[isess, "accuracy_openloop"] = decoder_dict["accuracy_openloop"]
+            results.at[isess, "conmat_closedloop"] = decoder_dict["conmat_closedloop"]
+            results.at[isess, "conmat_openloop"] = decoder_dict["conmat_openloop"]
+            isess+=1
+            print(f"SESSION {session}: decoder_results concatenated")
+        else:
+            print(f"ERROR: SESSION {session}: decoder_results not found")
+            continue
+    return results
+
+
+def bar_plot_ttest(group1, 
+                   group2,
+                   labels, 
+                   fig, 
+                   plot_x,
+                   plot_y,
+                   plot_width,
+                   plot_height
+                   ):
+    ax = fig.add_axes([plot_x, plot_y, plot_width, plot_height])
+    # Calculate means and standard deviations
+    mean1, mean2 = np.mean(group1), np.mean(group2)
+    std1, std2 = np.std(group1), np.std(group2)
+
+    # Calculate t-test
+    t_statistic, p_value = ttest_rel(group1, group2)
+
+    # Plot the data
+    bar1 = ax.bar(0, mean1, yerr=std1, capsize=10, width = 0.25, label=labels[0])
+    bar2 = ax.bar(0.3, mean2, yerr=std2, capsize=10, width = 0.25, label=labels[1])
+
+    # Add bracket and asterisk if significant
+    if p_value < 0.05:
+        star = '*'
+        if p_value < 0.01:
+           star = '**' 
+        elif p_value < 0.001:
+           star = '***' 
+        # Add square bracket
+        x0, x1 = bar1[0].get_x() + bar1[0].get_width() / 2, bar2[0].get_x() + bar2[0].get_width() / 2
+        y, h, col = max([mean1 + std1]), 0.05, 'k'
+        ax.plot([x0, x0, x1, x1], [y+h, y+2*h, y+2*h, y+h], lw=1.5, c=col)
+        # Add asterisk on top of bracket
+        ax.text((x0+x1)*.5, y+2*h,star, ha='center', va='bottom', fontsize=24)
+
+    # Set the x-axis labels and title
+    ax.set_xticks([0, 0.3])
+    ax.set_xticklabels([labels[0], labels[1]])
+
+    plotting_utils.despine()
+    
+    
+def dot_plot(
+    group1,
+    group2,
+    labels,
+    baselines,
+    fig,
+    group3=None,
+    group4=None,
+    markersize=5,
+    colors=['b','g'],
+    linewidth=4,
+    plot_x=0,
+    plot_y=1,
+    plot_width=1,
+    plot_height=1,
+    fontsize_dict={"title": 15, "label": 10, "tick": 10, "legend": 5},
+    
+):
+    ax = fig.add_axes([plot_x, plot_y, plot_width, plot_height])
+    ax.plot([0.3, 0.7], [group1, group2], f'{colors[0]}o-', alpha=0.7, label=labels[0], markersize=markersize, linewidth=linewidth)
+    ax.axhline(y=baselines[0], color=colors[0], linestyle='dotted', linewidth=linewidth)
+    t_statistic, p_value = ttest_rel(group1, group2)
+    print(f"t-test for {labels[0]}: {p_value}")
+    if (group3 is not None):
+        ax.plot([0.3, 0.7], [group3, group4], f'{colors[1]}o-', alpha=0.7, label=labels[1], markersize=markersize, linewidth=linewidth)
+        ax.axhline(y=baselines[1], color=colors[1], linestyle='dotted', linewidth=linewidth)
+        t_statistic, p_value = ttest_rel(group3, group4)
+        print(f"t-test for {labels[1]}: {p_value}")
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    plt.xticks([0.3, 0.7], fontsize=fontsize_dict['tick'])
+    plt.yticks(fontsize=fontsize_dict['tick'])
+    plt.xlim([0.1,0.9])
+    plt.ylim([0,1])
+    ax.set_xticklabels(['Closed \nloop', 'Open \nloop'], fontsize=fontsize_dict['label'])
+    plt.ylabel('Classification accuracy', fontsize=fontsize_dict['label'])
+    # ax.legend(fontsize=fontsize_dict['legend'])
+
+
+def calculate_average_confusion_matrix(results, conmat_column, sfx):
+    results[f"conmat_prop{sfx}"] = results[f"{conmat_column}{sfx}"].apply(lambda x: x/np.sum(x))
+    conmat_mean = np.mean(np.stack(results[f"conmat_prop{sfx}"]), axis=0)
+    return results, conmat_mean
+
+def plot_confusion_matrix(conmat,
+                            ax,
+                            vmax,
+                            fontsize_dict,
+                                  ):
+    ax.imshow(conmat, interpolation="nearest", cmap="Blues", vmax=vmax, vmin=0)
+    fmt = "d"
+    thresh = vmax / 2.0
+    for i, j in itertools.product(range(conmat.shape[0]), range(conmat.shape[1])):
+        display = conmat[i, j]
+        if display > 1:
+            display = f"{int(display)}"
+        else:
+            display = f"{display:.2f}"
+        plt.text(
+            j,
+            i,
+            display,
+            horizontalalignment="center",
+            color="white" if conmat[i, j] > thresh else "black",
+            fontsize=fontsize_dict["legend"],
+        )
+    plt.xlabel("Predicted depth class", fontsize=fontsize_dict["label"])
+    plt.ylabel("True depth class", fontsize=fontsize_dict["label"])
+
+def plot_closed_open_conmat(conmat_closed,
+                            conmat_open,
+                            normalize,
+                            fig,
+                            plot_x,
+                            plot_y,
+                            plot_width,
+                            plot_height,
+                            fontsize_dict,
+                            ):
+    if normalize:
+        conmat_closed = conmat_closed / conmat_closed.sum(axis=1)[:, np.newaxis]
+        conmat_open = conmat_open / conmat_open.sum(axis=1)[:, np.newaxis]
+    vmax = conmat_closed.max()
+    ax = fig.add_axes([plot_x, plot_y, plot_width/2*0.8, plot_height*0.8])
+    plot_confusion_matrix(conmat_closed, ax, vmax, fontsize_dict)
+    ax.set_title("Closed loop", fontsize=fontsize_dict["title"])
+    ax = fig.add_axes([plot_x+plot_width/2, plot_y, plot_width/2*0.8, plot_height*0.8])
+    plot_confusion_matrix(conmat_open, ax, vmax, fontsize_dict)
+    ax.set_title("Open loop", fontsize=fontsize_dict["title"])
---
+++ v1_depth_analysis/v1_manuscript_2023/get_session_list.py
@@ -0,0 +1,202 @@
+import flexiznam as flz
+
+def get_sessions(project, mouse_list, exclude_sessions=[]):
+    session_list = []
+    flexilims_session = flz.get_flexilims_session(project)
+    for mouse in mouse_list:
+        sessions_mouse = flz.get_children(parent_name=mouse, children_datatype="session", flexilims_session=flexilims_session).name.values.tolist()
+        session_list.append(sessions_mouse)
+    
+    # exclude any sessions from exclude_sessions
+    session_list = [session for i in session_list for session in i]
+    session_list = [session for session in session_list if session not in exclude_sessions]
+    
+    return session_list
+
+def get_all_sessions(project, mouse_list, closedloop_only=True, openloop_only=False):
+    session_list = []
+    
+    # Exclude non-V1 sessions
+    exclude_sessions = [
+        "PZAH6.4b_S20220428",
+        "PZAH6.4b_S20220510",
+        "PZAH6.4b_S20220526",
+        
+        "PZAG3.4f_S20220422",
+        "PZAG3.4f_S20220429",
+        "PZAG3.4f_S20220508",
+        "PZAG3.4f_S20220509",
+        "PZAG3.4f_S20220517",
+        "PZAG3.4f_S20220519",
+        
+        "PZAH8.2h_S20221208",
+        "PZAH8.2h_S20221213",
+        "PZAH8.2h_S20221215",
+        "PZAH8.2h_S20221216",
+        
+        "PZAH10.2d_S20230615",
+        "PZAH10.2d_S20230616",
+        "PZAH10.2d_S20230626",
+        
+        "PZAH10.2f_S20230525",
+        "PZAH10.2f_S20230601",
+        "PZAH10.2f_S20230614",
+        "PZAH10.2f_S20230626",
+        "PZAH10.2f_S20230705",
+        "PZAH10.2f_S20230801",
+        "PZAH10.2f_S20230804",
+        "PZAH10.2f_S20230808",
+        "PZAH10.2f_S20230811",
+        
+    ]
+    
+    # Exclude Faulty sessions --> DEBUG!!
+    exclude_sessions = exclude_sessions + [
+        "PZAH6.4b_S20220523",
+        
+        "PZAH8.2h_S20230126",
+        "PZAH8.2h_S20230310",
+        
+        "PZAH8.2f_S20230210",
+        "PZAH8.2f_S20230213",
+        "PZAH8.2f_S20230214",
+        "PZAH8.2f_S20230323",
+        
+        "PZAH10.2d_S20230623",
+        
+        "PZAH10.2f_S20230605",
+    ]
+    
+    # Exclude sessions with no proper settings
+    exclude_sessions = exclude_sessions + [
+        'PZAH6.4b_S20220401', # not proper settings
+        'PZAH6.4b_S20220411', # not proper settings
+        'PZAH6.4b_S20220513', # not proper settings
+        'PZAH8.2h_S20221208', # test
+        'PZAH8.2h_S20221213', # 10 depths
+        'PZAH8.2h_S20221215', # test
+        'PZAH8.2h_S20221216', # 10 depths
+        'PZAH8.2h_S20230411x1024', # test for 10x objective
+        'PZAH8.2h_S20230411x2048', # test for 10x objective
+        'PZAH8.2h_S20230411', # test for 10x objective
+        'PZAH8.2h_S20230410', # test for 10x objective
+        'PZAH8.2i_S20221208', # test
+        'PZAH8.2i_S20221209', # 10 depths
+        'PZAH8.2i_S20221213', # test
+        'PZAH8.2i_S20221215', # 10 depths
+        'PZAH8.2f_S20221206', # test
+        'PZAH8.2f_S20221209', # 10 depths
+        'PZAH8.2f_S20221212', # test
+        "PZAH10.2f_S20230911", # multiple cortical depths
+    ]
+    
+    # Exclude size control sessions
+    exclude_sessions = exclude_sessions + [
+        "PZAH10.2d_S20230822",
+        "PZAH10.2f_S20230815",
+        "PZAH10.2f_S20230907",
+    ]
+    
+    if (closedloop_only == True) and (openloop_only == True):
+        print("WARNING: Both closedloop_only and openloop_only are True. No session is returned.")
+        
+    if closedloop_only:
+        exclude_sessions = exclude_sessions + [
+            "PZAH6.4b_S20220519",
+            "PZAH6.4b_S20220524",
+            
+            "PZAG3.4f_S20220520",
+            "PZAG3.4f_S20220523",
+            "PZAG3.4f_S20220524",
+            "PZAG3.4f_S20220526",
+            "PZAG3.4f_S20220527",
+            
+            "PZAH8.2h_S20230224",
+            "PZAH8.2h_S20230302",
+            "PZAH8.2h_S20230303",
+            # "PZAH8.2h_S20230310",
+            
+            "PZAH8.2i_S20230203",
+            "PZAH8.2i_S20230209",
+            "PZAH8.2i_S20230216",
+            "PZAH8.2i_S20230220",
+            
+            "PZAH8.2f_S20230206",
+            # "PZAH8.2f_S20230210",
+            # "PZAH8.2f_S20230213",
+            # "PZAH8.2f_S20230214",
+            "PZAH8.2f_S20230223",
+            
+            "PZAH10.2d_S20230602",
+            "PZAH10.2d_S20230608",
+            "PZAH10.2d_S20230613",
+            "PZAH10.2d_S20230818",
+            "PZAH10.2d_S20230821",
+            "PZAH10.2d_S20230920",
+            "PZAH10.2d_S20230922",
+            
+            "PZAH10.2f_S20230606",
+            "PZAH10.2f_S20230609",
+            "PZAH10.2f_S20230615",
+            "PZAH10.2f_S20230623",
+            "PZAH10.2f_S20230627",
+            "PZAH10.2f_S20230822",
+            "PZAH10.2f_S20230908",
+            "PZAH10.2f_S20230924",                                
+        ]
+        session_list = session_list + (get_sessions(project, mouse_list, exclude_sessions=exclude_sessions))
+
+    elif openloop_only:
+        openloop_sessions = [
+            "PZAH6.4b_S20220519",
+            "PZAH6.4b_S20220524",
+            
+            "PZAG3.4f_S20220520",
+            "PZAG3.4f_S20220523",
+            "PZAG3.4f_S20220524",
+            "PZAG3.4f_S20220526",
+            "PZAG3.4f_S20220527",
+            
+            "PZAH8.2h_S20230224",
+            "PZAH8.2h_S20230302",
+            "PZAH8.2h_S20230303",
+            # "PZAH8.2h_S20230310",
+            
+            "PZAH8.2i_S20230203",
+            "PZAH8.2i_S20230209",
+            "PZAH8.2i_S20230216",
+            "PZAH8.2i_S20230220",
+            
+            "PZAH8.2f_S20230206",
+            # "PZAH8.2f_S20230210",
+            # "PZAH8.2f_S20230213",
+            # "PZAH8.2f_S20230214",
+            "PZAH8.2f_S20230223",
+            
+            "PZAH10.2d_S20230602",
+            "PZAH10.2d_S20230608",
+            "PZAH10.2d_S20230613",
+            "PZAH10.2d_S20230818",
+            "PZAH10.2d_S20230821",
+            "PZAH10.2d_S20230920",
+            "PZAH10.2d_S20230922",
+            
+            "PZAH10.2f_S20230606",
+            "PZAH10.2f_S20230609",
+            "PZAH10.2f_S20230615",
+            "PZAH10.2f_S20230623",
+            "PZAH10.2f_S20230627",
+            "PZAH10.2f_S20230822",
+            "PZAH10.2f_S20230908",
+            "PZAH10.2f_S20230924",                                
+        ]
+        
+        session_list = []
+        for mouse in mouse_list:
+            session_list = session_list + [session for session in openloop_sessions if mouse in session]
+            
+    elif (closedloop_only == False) and (openloop_only == False):
+        session_list = session_list + (get_sessions(project, mouse_list, exclude_sessions=exclude_sessions))
+    
+    session_list.sort()
+    return session_list
