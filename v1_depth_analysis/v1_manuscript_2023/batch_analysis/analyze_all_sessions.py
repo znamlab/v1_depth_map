22a727378dd064f11c071d1588855ec6362751f0
yiran25 <yiran.he@crick.ac.uk>
2024-03-14 15:00:13 +0000

    [feature] add option to run separate steps in analysis pipeline

--- v1_depth_analysis/v1_manuscript_2023/batch_analysis/analyze_all_sessions.py
+++ v1_depth_analysis/v1_manuscript_2023/batch_analysis/analyze_all_sessions.py
@@ -4,15 +4,21 @@ project = "hey2_3d-vision_foodres_20220101"
 pipeline_filename = "run_analysis_pipeline.sh"
 conflicts = "overwrite"
 session_list = [
-    "PZAH6.4b_S20220519",
+    # "PZAH10.2f_S20230817",
+    # "PZAH10.2f_S20230727",
+    # "PZAH10.2d_S20230704",
+    "PZAH8.2f_S20230313",
 ]
-
+run_rf = 1
+run_rsof_fit = 0
+run_plot = 1
 
 def main(
     project,
     session_list,
     pipeline_filename="run_analysis_pipeline.sh",
     conflicts="overwrite",
+    **kwargs,
 ):
     for session_name in session_list:
         if ("PZAH6.4b" in session_name) or ("PZAG3.4f" in session_name):
@@ -26,8 +32,9 @@ def main(
             pipeline_filename=pipeline_filename,
             conflicts=conflicts,
             photodiode_protocol=photodiode_protocol,
+            **kwargs,
         )
 
 
 if __name__ == "__main__":
-    main(project, session_list, pipeline_filename, conflicts)
+    main(project, session_list, pipeline_filename, conflicts, run_rf=run_rf, run_rsof_fit=run_rsof_fit, run_plot=run_plot)
