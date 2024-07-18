e30ce71cf075326f3ff3ee650b1e2cd21b947322
yiran25 <yiran.he@crick.ac.uk>
2024-07-18 11:26:13 +0100

    [feature] add utils for drawing scalebars

--- v1_depth_analysis/v1_manuscript_2023/common_utils.py
+++ v1_depth_analysis/v1_manuscript_2023/common_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import flexiznam as flz
 from cottage_analysis.pipelines import pipeline_utils
+import matplotlib.patches as patches
 
 
 def concatenate_all_neurons_df(
@@ -88,3 +89,28 @@ def dict2df(dict, df, cols, index):
 
 def find_columns_containing_string(df, substring):
     return [col for col in df.columns if substring in col]
+
+
+def get_unique_labels(ax):
+    handles, labels = ax.get_legend_handles_labels()
+    unique = {}
+    for handle, label in zip(handles, labels):
+        if label not in unique:
+            unique[label] = handle
+    return unique.values(), unique.keys()
+
+
+def draw_axis_scalebars(ax, scalebar_x, scalebar_y, scalebar_width, scalebar_height, scalebar_labels, xlim=None, ylim=None, label_fontsize=5, linewidth=1, right=True, bottom=True):
+    rect = patches.Rectangle((scalebar_x, scalebar_y), scalebar_width, scalebar_height, linewidth=linewidth, edgecolor='none', facecolor='none')
+    ax.add_patch(rect)
+    if right:
+        right_edge = patches.FancyBboxPatch((scalebar_x+scalebar_width, scalebar_y), 0, scalebar_height, boxstyle="square,pad=0", linewidth=linewidth, edgecolor='black', facecolor='none')
+        ax.add_patch(right_edge)
+        ax.text(scalebar_x + scalebar_width*1.2, scalebar_y + scalebar_height/2, scalebar_labels[1], fontsize=label_fontsize, ha='left', va='center')
+    ax.set_ylim(ylim)
+    if bottom:
+        bottom_edge = patches.FancyBboxPatch((scalebar_x, scalebar_y), scalebar_width, 0, boxstyle="square,pad=0", linewidth=linewidth, edgecolor='black', facecolor='none')
+        ax.add_patch(bottom_edge)
+        ax.text(scalebar_x + scalebar_width/2, scalebar_y-scalebar_height*0.7, scalebar_labels[0], fontsize=label_fontsize, ha='center', va='bottom')
+    ax.set_xlim(xlim)
+    ax.axis('off')
