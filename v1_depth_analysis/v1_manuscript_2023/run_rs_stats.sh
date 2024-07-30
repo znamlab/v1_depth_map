5d2ee65e88d44e629201c4c052f15d990f8e82c6
yiran25 <yiran.he@crick.ac.uk>
2024-07-30 11:37:58 +0100

    [feature] add bash script to run rs_stats and calculate raster plot

---
+++ v1_depth_analysis/v1_manuscript_2023/run_rs_stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=rs_stats
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=ncpu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/logs/rs_%j.log"
+
+. ~/.bash_profile
+ml purge
+
+ml Anaconda3/2020.07
+source activate base
+
+conda activate 2p_analysis_cottage2
+
+cd "/camp/home/hey2/home/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/"
+python calculate_rs_stats_all_sessions.py
