49713b745f1a1a521233427a4e914a753c382496
Antonin Blot <antonin.blot@crick.ac.uk>
2024-08-03 11:20:06 +0100

    [minor] skip over failed session for eye tracking

--- v1_depth_analysis/eye_tracking/analyse_all_sessions.py
+++ v1_depth_analysis/eye_tracking/analyse_all_sessions.py
@@ -1,3 +1,4 @@
+# %%
 import flexiznam as flz
 from v1_depth_analysis.v1_manuscript_2023 import get_session_list
 from v1_depth_analysis.eye_tracking.analysis import get_data
@@ -23,7 +24,7 @@ recording_type = ("two_photon",)
 protocol_base = "SpheresPermTubeReward"
 all_gaze_data = {}
 for session_name in sessions:
-    mouse, sess = session_name.split("_")
+    mouse, session = session_name.split("_")
     if mouse not in ["PZAH6.4b", "PZAG3.4f"]:
         continue
     print(f"Processing {session_name}")
@@ -43,12 +44,17 @@ for session_name in sessions:
         recordings = recordings[recordings["exclude_reason"].isna()]
 
     for recording, series in recordings.iterrows():
-        rec = recording[len(session_name) + 1 :]
-        gaze_data = get_data(PROJECT, mouse, sess, rec)
+        print(f"    recording {recording}")
+        recording = recording[len(session_name) + 1 :]
+        try:
+            gaze_data = get_data(PROJECT, mouse, session, recording)
+        except AttributeError as e:
+            print(f"    Error: {e} for {session_name}_{recording}")
+            continue
         rec_name = f"{session_name}_{recording}"
         gaze_data["rec_name"] = rec_name
         gaze_data["mouse"] = mouse
-        gaze_data["session"] = sess
+        gaze_data["session"] = session
         all_gaze_data[rec_name] = gaze_data
 # %%
 
@@ -72,3 +78,5 @@ std = gpby.aggregate(np.nanstd)
 mean = gpby.aggregate(np.nanmean)
 
 plt.scatter(mean, std)
+
+# %%
