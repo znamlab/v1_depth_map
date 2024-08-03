ff09b14bd359b50db1d4813e66247d71467bfa23
Antonin Blot <antonin.blot@crick.ac.uk>
2024-08-03 11:19:35 +0100

    [minor] update preprocess to new get_session signature

--- v1_depth_analysis/eye_tracking/preprocess_all_sessions.py
+++ v1_depth_analysis/eye_tracking/preprocess_all_sessions.py
@@ -2,20 +2,21 @@
 import flexiznam as flz
 import wayla
 from v1_depth_analysis.v1_manuscript_2023 import get_session_list
+
 # %%
 PROJECT = "hey2_3d-vision_foodres_20220101"
 flexilims_session = flz.get_flexilims_session(project_id=PROJECT)
 sessions = get_session_list.get_sessions(
     flexilims_session=flexilims_session,
     exclude_sessions=(),
-    closedloop_only=True,
-    openloop_only=False,
+    exclude_openloop=True,
+    exclude_pure_closedloop=False,
     v1_only=True,
 )
 print(f"Found {len(sessions)} sessions")
 # %%
-recording_type="two_photon",
-protocol_base="SpheresPermTubeReward"
+recording_type = ("two_photon",)
+protocol_base = "SpheresPermTubeReward"
 for session_name in sessions:
     print(f"Processing {session_name}")
     exp_session = flz.get_entity(
@@ -28,7 +29,7 @@ for session_name in sessions:
         query_value=recording_type,
         flexilims_session=flexilims_session,
     )
-    
+
     recordings = recordings[recordings.name.str.contains(protocol_base)]
     if "exclude_reason" in recordings.columns:
         recordings = recordings[recordings["exclude_reason"].isna()]
@@ -41,14 +42,14 @@ for session_name in sessions:
             dlc_model_tracking="headfixed_track_eye",
             camera_ds_name=f"{recording}_right_eye_camera",
             origin_id=None,
-            conflicts="overwrite",
+            conflicts="skip",
             use_slurm=True,
             dependency=None,
-            run_detect=True,
-            run_tracking=True,
-            run_ellipse=True,
+            run_detect=False,
+            run_tracking=False,
+            run_ellipse=False,
             run_reprojection=True,
             repro_kwargs=None,
         )
-        
+
     # %%
