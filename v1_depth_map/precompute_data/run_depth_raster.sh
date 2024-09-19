dfcc398a5402b7e9c8ed01083228c7c0e81f2d00
yiran25 <yiran.he@crick.ac.uk>
2024-09-19 11:20:18 +0100

    [chore] clean up precompute data scripts

--- v1_depth_map/precompute_data/run_depth_raster.sh
+++ v1_depth_map/precompute_data/run_depth_raster.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=figure
 #SBATCH --ntasks=1
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=16G
 #SBATCH --partition=ncpu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/logs/psth_%j.log"
+#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_map/precompute_data/logs/psth_%j.log"
 
 . ~/.bash_profile
 ml purge
@@ -16,5 +16,5 @@ source activate base
 
 conda activate 2p_analysis_cottage2
 
-cd "/camp/home/hey2/home/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/"
+cd "/camp/home/hey2/home/codes/3d-vision-analysis-2p/v1_depth_map/precompute_data/"
 python make_depth_tuning_raster.py 
