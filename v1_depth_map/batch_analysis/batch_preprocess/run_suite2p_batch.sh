efd4d652f419b51aa8cd3d7a6d0f2ffdad582006
yiran25 <yiran.he@crick.ac.uk>
2024-09-19 12:03:05 +0100

    [chore] change to new filepath

--- v1_depth_map/batch_analysis/batch_preprocess/run_suite2p_batch.sh
+++ v1_depth_map/batch_analysis/batch_preprocess/run_suite2p_batch.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=4G
 #SBATCH --partition=ncpu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/batch_preprocess/logs/batch_preprocess_%j.log"
+#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_map/batch_analysis/batch_preprocess/logs/batch_preprocess_%j.log"
 
 ml purge
 ml Anaconda3/2022.05
@@ -14,6 +14,6 @@ ml Anaconda3/2022.05
 source activate base
 
 conda activate 2p-preprocess
-cd "/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_analysis/v1_manuscript_2023/batch_preprocess/"
+cd "/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_map/batch_analysis/batch_preprocess/"
 echo Processing ${SESSION} in project ${PROJECT}
 python twop_preprocess_all_sessions.py
