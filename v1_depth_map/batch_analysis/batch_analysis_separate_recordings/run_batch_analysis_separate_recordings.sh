42d001ab676854854351b82db5c44f86c38fd10a
yiran25 <yiran.he@crick.ac.uk>
2024-09-19 12:20:03 +0100

    [rename] rename batch analysis script. change to correct file path

---
+++ v1_depth_map/batch_analysis/batch_analysis_separate_recordings/run_batch_analysis_separate_recordings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=batch_analysis
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=8G
+#SBATCH --partition=ncpu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output="/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_map/batch_analysis/batch_analysis_separate_recordings/logs/batch_analysis_%j.log"
+
+. ~/.bash_profile
+ml purge
+
+ml Anaconda3/2020.07
+source activate base
+
+conda activate 2p_analysis_cottage2
+cd "/camp/lab/znamenskiyp/home/users/hey2/codes/3d-vision-analysis-2p/v1_depth_map/batch_analysis/batch_analysis_separate_recordings/"
+python analyze_all_sessions_separate_recordings.py
