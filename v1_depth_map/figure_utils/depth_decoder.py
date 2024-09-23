[bugfix] fix calculation of CI for last bin of depth decoder error

v1_depth_map/figure_utils/depth_decoder.py
@@ -436,7 +436,7 @@ def plot_decoder_err_by_speeds(
     # bins that are within the highest bin (>1m/s)
     mean_err = np.concatenate([mean_err, [np.nanmean(err_speed_bins[highest_bin:])]])
     CI_low_highest, CI_high_highest = common_utils.get_bootstrap_ci(
-        np.nanmean(err_speed_bins[:, highest_bin:].T, axis=1)
+        np.nanmean(err_speed_bins[:, highest_bin:].T, axis = 0)
     )
     CI_low = np.concatenate([CI_low, CI_low_highest])
     CI_high = np.concatenate([CI_high, mean_err[-1] - CI_low_highest + mean_err[-1]])