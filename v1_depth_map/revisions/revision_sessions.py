3230832b74a91b2b7b25832dd3aa079b8cd80ee3
Antonin Blot <antonin.blot@crick.ac.uk>
2025-11-17 12:09:00 +0000

    [feature] plots for treadmill data

---
+++ v1_depth_map/revisions/revision_sessions.py
@@ -0,0 +1,40 @@
+"""
+List of session from revision experiments and the associated protocol
+
+Reading from flexilims is fine, but a bit annoying as all sessions have a spheretube
+protocol. Make it a bit simpler by listing them here.
+
+
+"""
+
+sessions = {
+    "PZAG17.3a_S20250402": "motor",
+    "PZAG17.3a_S20250319": "multidepth",
+    "PZAG17.3a_S20250306": "spheretube_5",
+    "PZAG17.3a_S20250305": "spheretube_4",
+    "PZAG17.3a_S20250303": "spheretube_3",
+    "PZAG17.3a_S20250228": "spheretube_2",
+    "PZAG17.3a_S20250227": "spheretube_1",
+    "PZAG16.3c_S20250401": "motor",
+    "PZAG16.3c_S20250317": "multidepth",
+    "PZAG16.3c_S20250313": "spheretube_5",
+    "PZAG16.3c_S20250310": "spheretube_4",
+    "PZAG16.3c_S20250221": "spheretube_3",
+    "PZAG16.3c_S20250220": "spheretube_2",
+    "PZAG16.3c_S20250219": "spheretube_1",
+    "PZAG16.3b_S20250401": "motor",
+    "PZAG16.3b_S20250317": "multidepth",
+    "PZAG16.3b_S20250313": "spheretube_5",
+    "PZAG16.3b_S20250310": "spheretube_4",
+    "PZAG16.3b_S20250226": "spheretube_3",
+    "PZAG16.3b_S20250225": "spheretube_2",
+    "PZAG16.3b_S20250224": "spheretube_1",
+    "PZAH17.1e_S20250403": "motor",
+    "PZAH17.1e_S20250318": "multidepth",
+    "PZAH17.1e_S20250313": "multidepth",
+    "PZAH17.1e_S20250311": "spheretube_5",
+    "PZAH17.1e_S20250307": "spheretube_4",
+    "PZAH17.1e_S20250306": "spheretube_3",
+    "PZAH17.1e_S20250305": "spheretube_2",
+    "PZAH17.1e_S20250304": "spheretube_1",
+}
