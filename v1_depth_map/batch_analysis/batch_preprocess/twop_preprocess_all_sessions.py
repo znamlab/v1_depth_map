1b9ca98e6054a56ed8c84ff1fe8fa0651c8d330c
ablot <antonin.blot@crick.ac.uk>
2026-06-05 09:52:14 +0100

    [feature] 2p preprocess dff without ASt

--- v1_depth_map/batch_analysis/batch_preprocess/twop_preprocess_all_sessions.py
+++ v1_depth_map/batch_analysis/batch_preprocess/twop_preprocess_all_sessions.py
@@ -1,40 +1,37 @@
 # %%
 import flexiznam as flz
-import numpy as np
-import pandas as pd
 from twop_preprocess import pipeline_utils
-from cottage_analysis.summary_analysis import get_session_list
+
 
 # Settings
 PROJECT = "hey2_3d-vision_foodres_20220101"
 flexilims_session = flz.get_flexilims_session(PROJECT)
-mouse_list = flz.get_entities("mouse", flexilims_session=flexilims_session)
-mouse_list = mouse_list[
-    mouse_list.name.isin(
-        [
-            "PZAH6.4b",
-            "PZAG3.4f",
-            "PZAH8.2h",
-            "PZAH8.2i",
-            "PZAH8.2f",
-            "PZAH10.2d",
-            "PZAH10.2f",
-        ]
+if False:
+    mouse_list = flz.get_entities("mouse", flexilims_session=flexilims_session)
+    mouse_list = mouse_list[
+        mouse_list.name.isin(
+            [
+                "PZAH6.4b",
+                "PZAG3.4f",
+                "PZAH8.2h",
+                "PZAH8.2i",
+                "PZAH8.2f",
+                "PZAH10.2d",
+                "PZAH10.2f",
+            ]
+        )
+    ]
+    SESSION_LIST = pipeline_utils.get_sessions(
+        flexilims_session,
+        exclude_openloop=False,
+        exclude_pure_closedloop=False,
+        v1_only=True,
+        trialnum_min=10,
+        mouse_list=mouse_list,
     )
-]
-SESSION_LIST = get_session_list.get_sessions(
-    flexilims_session,
-    exclude_openloop=False,
-    exclude_pure_closedloop=False,
-    v1_only=True,
-    trialnum_min=10,
-    mouse_list=mouse_list,
-)
-""" SESSION_LIST = [ "PZAH6.4b_S20220503",
-                "PZAH6.4b_S20220524",
-                "PZAG3.4f_S20220520",
-                "PZAG3.4f_S20220523"] """
-# %%
+else:
+    SESSION_LIST = ["PZAH10.2d_S20230822", "PZAH10.2f_S20230815", "PZAH10.2f_S20230907"]
+
 PIPELINE_FILENAME = "run_dff_noneuropil.sh"
 CONFLICTS = "overwrite"
 TAU = 0.7
@@ -43,7 +40,7 @@ TAU = 0.7
 def main(
     project,
     session_list,
-    pipeline_filename="run_suite2p_gpu.sh",
+    pipeline_filename=PIPELINE_FILENAME,
     conflicts="skip",
     tau=0.7,
 ):
--- v1_depth_map/batch_analysis/batch_size_control/size_control_all_sessions.py
+++ v1_depth_map/batch_analysis/batch_size_control/size_control_all_sessions.py
@@ -46,4 +46,6 @@ if __name__ == "__main__":
         conflicts,
         use_slurm,
         log_fname=log_fname,
+        use_annotated=False,
+        ast_neuropil=False,
     )
